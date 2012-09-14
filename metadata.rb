15e379071ba7e9924310c6505cbed033702111d0
Yevgeniy Viktorov <craftsman@yevgenko.me>
2012-09-15 02:27:12 +0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures php-fpm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 
 depends "php"
 depends "apt"
