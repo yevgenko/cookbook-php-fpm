7cd3369f57de379f57844f51c5878ef78e4b762d
Yevgeniy A. Viktorov <craftsman@yevgenko.me>
2014-05-02 03:10:46 +0300

    Version bump to 0.6.6 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures php-fpm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.5"
+version          "0.6.6"
 
 depends "apt"
 depends "yum"
