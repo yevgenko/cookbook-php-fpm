ffbcbfdeafd26703e02553eed90c184d90ba7944
Tim Smith <tsmith@chef.io>
2017-07-25 21:33:28 -0700

    Update contact information
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,10 @@
-name "php-fpm"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+name             "php-fpm"
+maintainer       "Chef Software, Inc."
+maintainer_email "cookbooks@chef.io"
 license          "Apache 2.0"
 description      "Installs/Configures php-fpm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.7.9"
-
-depends "apt"
-depends "yum", ">= 3.0"
 
 %w{ debian ubuntu centos redhat fedora amazon }.each do |os|
   supports os
