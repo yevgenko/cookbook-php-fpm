f5cb4986dc56d90a83bd38afa06700ed09864c26
Yevhen V <wik@bdhr.co>
2017-08-14 05:14:33 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures php-fpm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.9'
+version          '0.8.0'
 chef_version     '>= 12.14'
 
 %w( debian ubuntu centos redhat fedora amazon oracle scientific ).each do |os|
